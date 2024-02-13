3c937dc0c97cdf65900e23bc614604a38d9e429f
habibur18 <amarhabibur@gmail.com>
2024-02-13 22:34:59 +0600

    ffix

--- src/App/Pages/Blog/Blog.jsx
+++ src/App/Pages/Blog/Blog.jsx
@@ -23,7 +23,7 @@ const Blog = () => {
               </figure>
               <h3 className="text-2xl font-bold">Regional Manager & Limited time management.</h3>
               <p>{totalChar > 306 ? desc.slice(0, 306).replace(/\s+$/, "") + "..." : desc}</p>
-              <Link className="block bg-[#0198FE] text-white px-3 py-3 sm:px-5 sm:py-4 rounded-[36px] max-w-[176px] text-center mt-8" onClick={() => window.scrollTo(0, 0)} to={`/blogDetails`}>
+              <Link className="block bg-[#0198FE] text-white px-3 py-3 sm:px-5 sm:py-2 rounded-[36px] max-w-[176px] text-center mt-8" onClick={() => window.scrollTo(0, 0)} to={`/blogDetails`}>
                 Read More
               </Link>
             </article>
