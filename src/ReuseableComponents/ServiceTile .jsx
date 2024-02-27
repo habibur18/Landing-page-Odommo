6181f13bfe9cb54c1181e76a5cca0c5b03a1a4df
habibur18 <amarhabibur@gmail.com>
2024-02-27 13:07:28 +0600

    uppercase the moreservice btn

--- src/ReuseableComponents/ServiceTile .jsx
+++ src/ReuseableComponents/ServiceTile .jsx
@@ -82,13 +82,8 @@ export default function ServiceTile({ title, button }) {
         </div>
         {button && (
           <div className="text-center pt-5">
-            <Link onClick={() => window.scrollTo(0, 0)} to="/services" className="group bg-[#0198FE] text-white px-3 py-3 sm:px-5 sm:py-4 rounded-[36px] flex justify-center gap-2 hover:bg-white hover:text-[#0198FE] hover:ring-2 hover:ring-[#0198FE] duration-300 group max-w-[200px] mx-auto">
+            <Link onClick={() => window.scrollTo(0, 0)} to="/services" className="group bg-[#0198FE] text-white px-3 py-3 sm:px-5 sm:py-4 rounded-[36px] flex justify-center gap-2 hover:bg-white hover:text-[#0198FE] hover:ring-2 hover:ring-[#0198FE] duration-300 group max-w-[200px] mx-auto uppercase">
               More Services
-              <span className="transform  inline-block translate-x-0 group-hover:translate-x-2 duration-300">
-                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 25 25" fill="none" className="stroke-current">
-                  <path d="M7.5 17.5L17.5 7.5M17.5 7.5H7.5M17.5 7.5V17.5" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-                </svg>
-              </span>
             </Link>
           </div>
         )}
